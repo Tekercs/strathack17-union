f11da143e4c9639e3bb06d61460b641955c0d569
Bence Sebestyen <bence.sebestyen.2015@uni.strath.ac.uk>
2017-02-12 10:09:37 +0000

    slideshow added to add view

--- resources/views/wrapper.blade.php
+++ resources/views/wrapper.blade.php
@@ -5,6 +5,23 @@
     <title>UNION index</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        #myCarousel
+        {
+            margin-bottom: 20px;
+        }
+        .carousel-inner > .item > img,
+        .carousel-inner > .item > a > img {
+            width: 70%;
+            margin: auto;
+        }
+    </style>
     <link rel="stylesheet" href="/assets/css/main.css" />
     <link rel="stylesheet" href="/css/style.css" />
 </head>
